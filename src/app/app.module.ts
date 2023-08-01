88a67296d3755f1b94b205a7b489c137b946a2f8
WildBarreto <wildsbsrreto@gmail.com>
2023-08-01 08:49:18 -0300

    product-read

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,11 @@ import { ProductCrudComponent } from "./views/product-crud/product-crud.componen
 import { FormsModule } from "@angular/forms";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -33,6 +38,8 @@ import { MatInputModule } from "@angular/material/input";
     HomeComponent,
     ProductCrudComponent,
     ProductCreateComponent,
+    ProductReadComponent,
+    ProductRead2Component,
   ],
   imports: [
     BrowserModule,
@@ -48,6 +55,9 @@ import { MatInputModule } from "@angular/material/input";
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
